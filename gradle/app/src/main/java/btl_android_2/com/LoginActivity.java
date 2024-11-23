b50245126e229c1bbe2a385e4e3248311496ce96
XuanPhuong39 <trinhxuanphuong39@gmail.com>
2024-11-23 14:32:45 +0700

    merge pull requert #1

---
+++ gradle/app/src/main/java/btl_android_2/com/LoginActivity.java
@@ -0,0 +1,68 @@
+package btl_android_2.com;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.text.TextUtils;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+
+import btl_android_2.com.ui.DBSQLite.DatabaseHelper;
+import btl_android_2.com.ui.trangChu.fragment_trangchu;
+
+public class LoginActivity extends AppCompatActivity {
+    DatabaseHelper myDb;
+    EditText editUserName, editPassword;
+    Button btnDongY, btnDangKy;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.layout_dangnhap);
+        myDb = new DatabaseHelper(this);
+
+
+        editUserName = findViewById(R.id.editUserName);
+        editPassword = findViewById(R.id.editPassword);
+        btnDongY = findViewById(R.id.btnDongY);
+        btnDangKy = findViewById(R.id.btnDangKy);
+
+        // Handle the "Đồng ý" button click
+        btnDongY.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String username = editUserName.getText().toString().trim();
+                String password = editPassword.getText().toString().trim();
+
+                if (TextUtils.isEmpty(username) || TextUtils.isEmpty(password)) {
+                    Toast.makeText(LoginActivity.this, "Vui lòng nhập tên đăng nhập và mật khẩu", Toast.LENGTH_SHORT).show();
+                    return;
+                }
+
+                // Validate the username and password
+                boolean isValid = myDb.checkUser(username, password);
+                if (isValid) {
+
+                    Toast.makeText(LoginActivity.this, "Đăng nhập thành công!", Toast.LENGTH_SHORT).show();
+                    Intent intent = new Intent(LoginActivity.this, MainActivity.class);
+                    startActivity(intent); // Bắt đầu hoạt động
+                    finish();
+                } else {
+                    Toast.makeText(LoginActivity.this, "Tên đăng nhập hoặc mật khẩu không chính xác", Toast.LENGTH_SHORT).show();
+                }
+            }
+        });
+
+        // Handle the "Đăng ký" button click
+        btnDangKy.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(LoginActivity.this, RegisterActivity.class);
+                startActivity(intent);
+            }
+        });
+    }
+}
---
+++ gradle/app/src/main/java/btl_android_2/com/MainActivity.java
@@ -0,0 +1,158 @@
+package btl_android_2.com;
+
+import android.database.Cursor;
+import android.database.sqlite.SQLiteDatabase;
+import android.os.Bundle;
+import android.view.View;
+import android.view.Menu;
+import android.widget.TextView;
+
+import androidx.appcompat.widget.Toolbar;
+
+import com.google.android.material.snackbar.Snackbar;
+import com.google.android.material.navigation.NavigationView;
+
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentTransaction;
+import androidx.navigation.NavController;
+import androidx.navigation.Navigation;
+import androidx.navigation.ui.AppBarConfiguration;
+import androidx.navigation.ui.NavigationUI;
+import androidx.drawerlayout.widget.DrawerLayout;
+import androidx.appcompat.app.AppCompatActivity;
+
+import btl_android_2.com.databinding.ActivityMainBinding;
+import btl_android_2.com.ui.DBSQLite.DatabaseHelper;
+import btl_android_2.com.ui.dangBan.fragment_dangban;
+import btl_android_2.com.ui.trangChu.fragment_trangchu;
+
+public class MainActivity extends AppCompatActivity {
+
+    private AppBarConfiguration mAppBarConfiguration;
+    private DatabaseHelper dbHelper;
+
+    private TextView txttentaikhoan;
+    private TextView txttennguoidung;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+//        // Thêm fragment_trangchu vào Activity
+//        FragmentManager fragmentManager = getSupportFragmentManager();
+//        FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
+//        fragment_trangchu fragment = new fragment_trangchu();
+//        fragmentTransaction.replace(R.id.fragment_container, fragment);
+//        fragmentTransaction.commit();
+
+        Toolbar toolbar = findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        DrawerLayout drawerLayout = findViewById(R.id.drawer_layout);
+        NavigationView navigationView = findViewById(R.id.nav_view);
+
+        mAppBarConfiguration = new AppBarConfiguration.Builder(
+                R.id.nav_trangchu, R.id.nav_tailieu, R.id.nav_danhsach, R.id.nav_chiase, R.id.nav_dang)
+                .setOpenableLayout(drawerLayout)
+                .build();
+
+        NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment_content_main);
+        NavigationUI.setupActionBarWithNavController(this, navController, mAppBarConfiguration);
+        NavigationUI.setupWithNavController(navigationView, navController);
+//        if (savedInstanceState == null) {
+//            FragmentTransaction transaction = getSupportFragmentManager().beginTransaction();
+//            transaction.add(R.id.fragment_container, new fragment_dangban());
+//            transaction.commit();
+//        }
+
+
+/////////////////////////////////////////////////////////// test database
+        //Tham chiếu database
+        dbHelper = DatabaseHelper.getInstance(this);
+
+        SQLiteDatabase db = dbHelper.getWritableDatabase();
+
+        //ví dụ thêm dữ liệu vào database bảng Account. Chạy thử thấy hiển thị được thì xóa đi
+        String addContent = "INSERT INTO Account (email, tenDangNhap, tenNguoiDung, matKhau, isAdmin, soDienThoai) VALUES (?, ?, ?, ?, ?, ?)";
+        db.execSQL( addContent,
+                new Object[]{"example@example.com", "gitclone", "Example User", "password123", 0, "0123456789"});
+        txttentaikhoan = findViewById(R.id.txttentaikhoan);
+        txttennguoidung = findViewById(R.id.txttennguoidung);
+        String[] projection = {
+                "id",
+                "email",
+                "tenDangNhap",
+                "tenNguoiDung",
+                "matKhau",
+                "isAdmin",
+                "soDienThoai"
+        };
+
+        String orderBy = "id DESC";
+        String limit = "1";
+
+        //tạo con trỏ
+        Cursor cursor = db.query(
+                "Account",
+                projection,
+                null, // No selection clause
+                null, // No selection arguments
+                null, // No group by
+                null, // No having
+                orderBy, // Order by id descending
+                limit // Limit to 1 result
+        );
+
+        // Lấy ra dữ liệu hiển thị ở 2 text view mới thêm ở trong màn hình thêm
+        if (cursor.moveToFirst()) {
+            // Extract data from cursor
+            int id = cursor.getInt(cursor.getColumnIndexOrThrow("id"));
+            String email = cursor.getString(cursor.getColumnIndexOrThrow("email"));
+            String tenDangNhap = cursor.getString(cursor.getColumnIndexOrThrow("tenDangNhap"));
+            String tenNguoiDung = cursor.getString(cursor.getColumnIndexOrThrow("tenNguoiDung"));
+            String matKhau = cursor.getString(cursor.getColumnIndexOrThrow("matKhau"));
+            String isAdmin = cursor.getString(cursor.getColumnIndexOrThrow("isAdmin"));
+            String soDienThoai = cursor.getString(cursor.getColumnIndexOrThrow("soDienThoai"));
+
+            // Display data (for example, log it or set it to a TextView)
+          txttentaikhoan.setText(tenDangNhap);
+          txttennguoidung.setText(tenNguoiDung);
+        }
+
+        cursor.close();
+        // xóa đến đây nếu chạy thấy hiển thị được
+/////////////////////////////////////////////////////////////////////////////////////////////
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onSupportNavigateUp() {
+        NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment_content_main);
+        return NavigationUI.navigateUp(navController, mAppBarConfiguration)
+                || super.onSupportNavigateUp();
+    }
+    private void getview(){
+//
+//            FragmentTransaction transaction = getSupportFragmentManager().beginTransaction();
+//            transaction.replace(R.id.fragment_container, new fragment_dangban());
+//            transaction.commit();
+
+    }
+
+    private void themAccount(String email, String tenDangNhap, String tenNguoiDung, String matKhau, Integer isAdmin, String soDienThoai)
+    {
+        SQLiteDatabase db = dbHelper.getWritableDatabase();
+        String sql = "INSERT INTO Account (email, tenDangNhap, tenNguoiDung, matKhau, isAdmin, soDienThoai) VALUES (?, ?, ?, ?, ?, ?)";
+        Object[] bindArgs = {email, tenDangNhap, tenNguoiDung, matKhau, isAdmin, soDienThoai};
+        db.execSQL(sql, bindArgs);
+    }
+
+
+}
